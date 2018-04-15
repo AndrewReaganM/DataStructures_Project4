fa48e18deb2b3ff8d103f3db1b23d7c60cb651c0
Andrew Massey <amassey107@gmail.com>
2018-04-14 23:03:15 -0500

    WOW THX NATAN

--- main.cpp
+++ main.cpp
@@ -8,8 +8,41 @@ using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
+    char status = 'X';
+    string keyword;
     ttree t = *new ttree();
+    cout << "### Autocorrect Tree ###" << endl;
+    cout << "# Please enter a valid command." << endl;
+    while(cin >> status) {
 
+        switch (status) {
+            case 'I' :
+//                getline(cin, keyword);
+                cin >> keyword;
+                t.insert(keyword);
+                break;
+            case 'S' :
+                //search
+                break;
+            case 'D' :
+                cout << t;
+                break;
+
+        }
+        cout << "# Please enter a valid command." << endl;
+    }
+
+
+
+
+    /*
     t.insert("HELLO");
+    t.insert("HAMS");
+    t.insert("APRIL");
+    t.insert("AMY");
+    t.insert("KATIE");
+    t.insert("KENNETH");
+    t.insert("KATHERINE");
+    t.insert("KAKE");
+     */
 }
--- ttree.h
+++ ttree.h
@@ -18,18 +18,19 @@ private:
     int _currentDepth;
 public:
     ttree();
-    ttree(int maxDepth, int depthOffset);
+    ttree(int maxDepth, int depth);
     ~ttree();
 
     void insert(string key);
     void search(string key);
-    friend ostream& operator << (std::ostream& os, const ttree& tree);
-
+    friend ostream& operator<< (std::ostream& os, ttree& tree);
 
     int toInt(char c);
     char toChar(int i);
 
 };
+
+
 
 class tnode {
 private:
@@ -41,14 +42,33 @@ public:
     int findWord(string w);
     bool insert(string w, int level);
     list<string>* getWords();
-    void put(string key);
+    bool put(string key);
     int removeWord(string w);
     void clearWords();
     int commonPrefix(string o, string n);
-    ttree getNext();
-    void setNext(ttree t);
+    ttree* getNext();
+    void setNext(ttree* t);
     bool wordsIsNull();
-    friend ostream& operator << (ostream& os, const tnode& t);
+    bool nextIsNull();
+    void display();
+    friend ostream& operator << (ostream& os, tnode& t) {
+
+        cout << "Printing Node" << endl;
+
+        if(t.getWords() != nullptr)
+        {
+            auto it = t._words->begin();
+            auto endIt = t._words->end();
+
+            while (it != endIt) {
+                os << (*it) << " ";
+                ++it;
+            }
+            os << endl;
+            os << t._nextLevel;
+        }
+        return os;
+    }
 };
 
 
@@ -58,51 +78,67 @@ ttree::ttree() {
     _tnodes = new tnode[26];
 
 }
-ttree::ttree(int maxDepth, int depthOffset) {
+ttree::ttree(int maxDepth, int depth) {
     _maxDepth = maxDepth;
-    _currentDepth = 1;
+    _currentDepth = depth;
     _tnodes = new tnode[26];
+
 }
 /**
  * Insert method for the tree.
  * @param key the word to be inserted.
  */
 void ttree::insert(string key) {
-    ttree* newLevel;
+    if (_currentDepth > key.length()) throw std::runtime_error("RIP IN PEACE");
+
     int index = toInt(key.at(_currentDepth - 1));
-    if(_tnodes[index].wordsIsNull())
-    {
-        cout <<  "Next Level!" << endl;
-        _tnodes[index].getNext().insert(key);
-    }
+    if (index < 0 )
+        throw std::runtime_error("RIP IN PEACE IN PEACE");
+
+    if(((_currentDepth+1) > _maxDepth) || key.length() == _currentDepth)
+        _tnodes[index].put(key);
+    else if (!(_tnodes[index].nextIsNull())) {
+            _tnodes[index].getNext()->insert(key);
+        }
     else
     {
-        if(_tnodes[index].insert(key, _currentDepth) == false)
+        if(!_tnodes[index].insert(key, _currentDepth))
         {
-            if((_currentDepth+1) > _maxDepth) _tnodes[index].put(key);
+            ttree* newLevel = new ttree(_maxDepth, _currentDepth + 1);
+            list<string> oldWords = (*(_tnodes[index].getWords()));
 
-            else
+            for (string s : oldWords)
             {
-                newLevel = new ttree(_maxDepth, _currentDepth + 1);
-                list<string> oldWords = (*(_tnodes[index].getWords()));
-
-                auto iter = oldWords.begin();
-
-                for(; iter != oldWords.end(); ++iter)
-                {
-                    newLevel->insert(*iter);
-                }
-                newLevel->insert(key);
-
-                _tnodes[index].setNext(*newLevel);
-                _tnodes[index].clearWords();
+                newLevel->insert(s);
             }
+            newLevel->insert(key);
+
+            _tnodes[index].setNext(newLevel);
+            _tnodes[index].clearWords();
+
         }
     }
+}
 
+ostream& operator << (std::ostream& os, ttree& tree)
+{
+    cout << "Printing Tree" << endl;
+    for(int i=0; i< 26; i++)
+    {
+        if (tree._tnodes[i].getWords() == nullptr && tree._tnodes[i].getNext() == nullptr)
+        {
 
+        }
+        else
+        {
+            //cout << "WHAT";
+            for(int i=0; i < 3*(tree._currentDepth-1); i++) os << ' ';
 
-
+            os << tree.toChar(i);
+            os << tree._tnodes[i];
+        }
+    }
+    return os;
 }
 /**
  * Function to return the int value of a letter.
@@ -110,7 +146,7 @@ void ttree::insert(string key) {
  * @return int representing the character input.
  */
 int ttree::toInt(char c) {
-    return ((int) c) - 65;
+    return ((int) c) - (int)'A';
 }
 /**
  * Function ot return the int value of a letter.
@@ -118,7 +154,7 @@ int ttree::toInt(char c) {
  * @return char representing the character number input.
  */
 char ttree::toChar(int i) {
-    return((char) (i+65));
+    return((char) (i+'A'));
 }
 
 ttree::~ttree() {
@@ -131,8 +167,8 @@ ttree::~ttree() {
  * Default constructor for tnode.
  */
 tnode::tnode() {
-    _nextLevel = NULL;
-    _words = NULL;
+    _nextLevel = nullptr;
+    _words = nullptr;
 }
 
 /**
@@ -141,10 +177,13 @@ tnode::tnode() {
  */
 bool tnode::insert(string w, int level)
 {
-    if (_words == NULL) {
-        _words = new list<string>(); //Initialize the list.
-        _words->push_back(w);
+    //cout << "Level " << level << " insert" << endl;
+    if(_words == nullptr && _nextLevel == nullptr)
+    {
+        put(w);
+        return true;
     }
+
     //Check Validity of Insert
     auto it = _words->begin();
     auto endIt = _words->end();
@@ -155,16 +194,11 @@ bool tnode::insert(string w, int level)
         {
             return false;
         }
+        ++it;
     }
-    if (_words == NULL) {
-        _words = new list<string>(); //Initialize the list.
-    }
-
-    it = _words->begin();
-    auto nextIt = _words->begin();
-    ++nextIt;
 
     put(w); //Insert the given data.
+    display();
     return true;
 }
 /**
@@ -173,7 +207,7 @@ bool tnode::insert(string w, int level)
  * @return int representing method return status.
  */
 int tnode::findWord(string w) {
-    if (_words == NULL) return -1;
+    if (_words == nullptr) return -1;
     auto it = _words->begin();
     auto endIt = _words->end();
     //--endIt;
@@ -192,9 +226,8 @@ int tnode::findWord(string w) {
  * Fully clears the _words variable in the node.
  */
 void tnode::clearWords() {
-    if (_words == NULL) return;
-    delete _words;
-    _words = NULL;
+    if (_words == nullptr) return;
+    _words = nullptr;
 }
 
 /**
@@ -205,18 +238,37 @@ list<string>* tnode::getWords() {
     return _words;
 }
 
-void tnode::put(string key)
+bool tnode::put(string key)
 {
+    //cout << "put" << endl;
+    if(_words == nullptr)
+    {
+        _words = new list<string>;
+        _words->push_back(key);
+        return true;
+    }
     auto it = _words->begin();
     auto endIt = _words->end();
     while(it != endIt)
     {
-        if(!((*it)< key))
+        if(key < ((*it)))
         {
+            //cout << key << " is less than " << (*it) << endl;
             _words->insert(it, key);
+            //cout << "Size: " << _words->size() << endl;
+            return true;
         }
+
         ++it;
     }
+    if(it == endIt)
+    {
+        _words->push_back(key);
+        //cout << "Size: " << _words->size() << endl;
+        return true;
+    }
+    cout << "PUT FAILED" << endl;
+    return false;
 }
 
 /**
@@ -225,7 +277,7 @@ void tnode::put(string key)
  * @return 0 if successful, -1 if unsuccessful or list empty.
  */
 int tnode::removeWord(string w) {
-    if (_words == NULL) return -1;
+    if (_words == nullptr) return -1;
     auto it = _words->begin();
     auto endIt = _words->end();
 
@@ -256,26 +308,41 @@ int tnode::commonPrefix(string o, string n) {
         {
             ++count;
         }
+        else return count;
         ++i;
     }
+    //cout << count << endl;
     return count;
 }
 
-void tnode::setNext(ttree t) {
-    _nextLevel = &t;
+void tnode::setNext(ttree* t) {
+    _nextLevel = t;
 }
 
 /**
  * Method to obtain a pointer to the next level.
  * @return ttree pointer representing the next level.
  */
-ttree tnode::getNext() {
-        return *_nextLevel;
+ttree* tnode::getNext() {
+        return _nextLevel;
 }
 
 bool tnode::wordsIsNull()
 {
-    return (_words != NULL);
+    return (_words == nullptr);
+}
+bool tnode::nextIsNull(){
+    return (_nextLevel == nullptr);
+}
+void tnode::display() {
+    auto it = _words->begin();
+    auto endIt = _words->end();
+
+    while(it != endIt)
+    {
+        cout << (*it) << " ";
+        ++it;
+    }
 }
 
 tnode::~tnode() {
